613babd14ecf24d5759702f488b93aa5cdaebe88
soywiz <soywiz@gmail.com>
2023-01-30 21:25:55 +0100

    Update to latest version of KorGE

--- korge-spine/src/commonMain/kotlin/com/esotericsoftware/spine/SpineRegion.kt
+++ korge-spine/src/commonMain/kotlin/com/esotericsoftware/spine/SpineRegion.kt
@@ -1,22 +1,25 @@
 package com.esotericsoftware.spine
 
 import com.soywiz.korim.atlas.*
+import com.soywiz.korim.bitmap.*
 import com.soywiz.korim.format.ImageOrientation
+import com.soywiz.korma.geom.slice.*
 
 class SpineRegion(val entry: Atlas.Entry) {
-    val bmpSlice = entry.slice
-    val bmp = bmpSlice.bmpBase
-    val texture = bmp
-    var rotate = entry.info.imageOrientation != ImageOrientation.ORIGINAL
-    val u: Float = bmpSlice.tl_x
-    val u2: Float = bmpSlice.br_x
-    val v: Float = if (rotate) bmpSlice.br_y else bmpSlice.tl_y
-    val v2: Float = if (rotate) bmpSlice.tl_y else bmpSlice.br_y
-    var offsetX = (entry.info.virtFrame?.x ?: 0).toFloat()
-    var offsetY = (entry.info.virtFrame?.y ?: 0).toFloat()
-    var originalWidth = (entry.info.virtFrame?.w ?: entry.info.srcWidth).toFloat()
-    var originalHeight = (entry.info.virtFrame?.h ?: entry.info.srcHeight).toFloat()
-    var packedWidth = entry.info.srcWidth.toFloat()
-    var packedHeight = entry.info.srcHeight.toFloat()
-    var degrees = if (rotate) 90 else 0
+    val bmpSlice: BmpSlice = entry.slice
+    val bmp: Bitmap = bmpSlice.base
+    val coords: RectCoords = bmpSlice.coords
+    val texture: Bitmap = bmp
+    var rotate: Boolean = entry.info.imageOrientation != ImageOrientation.ORIGINAL
+    val u: Float = coords.tlX
+    val u2: Float = coords.brX
+    val v: Float = if (rotate) bmpSlice.brY else coords.tlY
+    val v2: Float = if (rotate) bmpSlice.tlY else coords.brY
+    var offsetX: Float = (entry.info.virtFrame?.x ?: 0).toFloat()
+    var offsetY: Float = (entry.info.virtFrame?.y ?: 0).toFloat()
+    var originalWidth: Float = (entry.info.virtFrame?.w ?: entry.info.frame.w).toFloat()
+    var originalHeight: Float = (entry.info.virtFrame?.h ?: entry.info.frame.h).toFloat()
+    var packedWidth: Float = entry.info.frame.w.toFloat()
+    var packedHeight: Float = entry.info.frame.h.toFloat()
+    var degrees: Int = if (rotate) 90 else 0
 }
